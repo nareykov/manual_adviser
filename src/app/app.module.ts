871eb4e16b71dba806615a340c2a21871e2a7db7
nareykov <nareykov@mail.ru>
2017-07-21 13:50:15 +0300

    search & popular views

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,32 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {Routes, RouterModule} from '@angular/router';
 
 import { AppComponent } from './app.component';
+import {SideComponent} from './side/side.component';
+import {MainComponent} from './main/main.component';
+import {HeadComponent} from './head/head.component';
+import { SearchComponent } from './main/search/search.component';
+import { PopularComponent } from './main/popular/popular.component';
+
+const appRoutes: Routes = [
+  {path: 'search', component: SearchComponent},
+  {path: 'popular', component: PopularComponent},
+  {path: '**', redirectTo: '/popular'}
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SideComponent,
+    MainComponent,
+    HeadComponent,
+    SearchComponent,
+    PopularComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
