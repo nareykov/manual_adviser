e4a988074bbea25809720c01aca99d2d6328ca55
Andrey-2310 <andreyredkovskiy@mail.ru>
2017-07-24 20:49:46 +0300

    users added

--- src/app/main/search/search.component.ts
+++ src/app/main/search/search.component.ts
@@ -1,15 +1,24 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {User} from './user';
+import {UserService} from './user.service';
 
 @Component({
   selector: 'app-search',
   templateUrl: './search.component.html',
-  styleUrls: ['./search.component.css']
+  styleUrls: ['./search.component.css'],
+  providers: [UserService]
 })
 export class SearchComponent implements OnInit {
 
-  constructor() { }
+  users: Array<User>;
 
-  ngOnInit() {
+  constructor(private userService: UserService) {
   }
 
+  ngOnInit(): void {
+    this.getUsers();
+  }
+  getUsers() {
+    this.userService.getUserArray().subscribe((data) => this.users = data);
+  }
 }
---
+++ src/app/main/search/user.ts
@@ -0,0 +1,6 @@
+export class User {
+  username: string;
+  image: string;
+  commentAmmount: number;
+  postAmmount: number;
+}
